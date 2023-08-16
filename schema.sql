4e70fcb351b84794c3793210ab06a16db8318442
MARVIN <o.silverygold@gmail.com>
2023-08-16 22:00:51 +0300

    Add solutions for queries, data and schema

--- schema.sql
+++ schema.sql
@@ -30,4 +30,38 @@ ADD COLUMN owner_id integer,
 ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id) ON DELETE CASCADE,
 ADD CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id) ON DELETE CASCADE;
 
+CREATE TABLE vets (
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255),
+  age integer,
+  date_of_graduation date
+);
 
+CREATE TABLE specializations (
+  species_id integer,
+  vet_id integer,
+  CONSTRAINT species_fk FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE,
+  CONSTRAINT vets_fk FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE CASCADE
+);
+
+CREATE TABLE visits (
+    animal_id integer,
+    vet_id integer,
+    visit_date date,
+    CONSTRAINT animals_fk FOREIGN KEY (animal_id) REFERENCES animals(id) ON DELETE CASCADE,
+    CONSTRAINT vets_fk FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE CASCADE
+);
+
+SELECT species.name AS suggested_specialty, COUNT(visits.animal_id) AS visit_count
+FROM species
+INNER JOIN specializations ON species.id = specializations.species_id
+INNER JOIN vets ON specializations.vet_id = vets.id
+INNER JOIN visits ON specializations.vet_id = visits.vet_id AND visits.animal_id IN (
+    SELECT animal_id
+    FROM visits
+    INNER JOIN vets ON visits.vet_id = vets.id
+    WHERE vets.name = 'Maisy Smith'
+)
+GROUP BY species.id, species.name
+ORDER BY visit_count DESC
+LIMIT 1;
